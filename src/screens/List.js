b494d1a370a86080e3edc761a5258db7a098cc64
Joshua Simon <joshy.d.simon@gmail.com>
2023-03-23 18:59:12 +1300

    date formatting

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,9 +1,18 @@
 import { useGigs } from "../hooks/useGigs";
 import GigsToday from "../components/GigsToday";
+import { format } from "date-fns";
 
 const List = () => {
+  const currentDateMs = Date.now();
+  const formattedDay = format(new Date(currentDateMs), "EEEE");
+  const formattedWeek = format(new Date(currentDateMs), "LLLL do Y");
+
   return (
-    <div>
+    <div className="list">
+      <div className="gigsToday_date">
+        <p className="gigsToday_date_day">{formattedDay}</p>
+        <p className="gigsToday_date_week">{formattedWeek}</p>
+      </div>
       <div className="listButtons">
         <button>Gigs Today</button>
         <button>Gigs This Week</button>
