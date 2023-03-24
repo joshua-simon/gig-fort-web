b707a43f5e6066ee294563e161c7f84612f7459d
Joshua Simon <joshy.d.simon@gmail.com>
2023-03-25 09:33:23 +1300

    map added

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,9 +1,41 @@
-
+import { useState } from 'react'
+import { GoogleMap,useJsApiLoader } from '@react-google-maps/api';
+import { mapStyle } from '../util/mapStyle';
 
 const Map = () => {
 
+    const { isLoaded } = useJsApiLoader({
+        id:'google-map-script',
+        googleMapsApiKey:"AIzaSyDsI5Nfl4xBcf9qsmtQAFzUkqMMmBC9WCw"
+        
+    })
+
+    const center = {
+        lat:-41.294,
+        lng:174.777
+    }
+
+    const containerStyle = {
+        width: '373px',
+        height: '410px',
+        borderRadius: '26px'
+      };
+
+    const map = isLoaded ? (
+        <div className="map-container">
+    <GoogleMap
+        zoom={14}
+        center = {center}
+        mapContainerStyle={containerStyle}
+        options = {{styles:mapStyle}}
+        >
+
+        </GoogleMap>
+        </div>
+    ) : <></>
+
     return ( 
-        <div>Map</div>
+        <div>{map}</div>
      );
 }
  
