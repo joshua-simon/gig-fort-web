4fbfc498c3ff4d1c844fe7089be51a6e580c65ec
Joshua Simon <joshy.d.simon@gmail.com>
2023-03-25 10:54:15 +1300

    markers showing

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,42 +1,65 @@
-import { useState } from 'react'
-import { GoogleMap,useJsApiLoader } from '@react-google-maps/api';
-import { mapStyle } from '../util/mapStyle';
+import { useState } from "react";
+import { GoogleMap, useJsApiLoader,Marker } from "@react-google-maps/api";
+import { mapStyle } from "../util/mapStyle";
+import logo from "../assets/logo.png";
+import { getGigsToday } from "../util/helperFunctions";
+import { useGigs } from "../hooks/useGigs";
 
 const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDsI5Nfl4xBcf9qsmtQAFzUkqMMmBC9WCw",
+  });
 
-    const { isLoaded } = useJsApiLoader({
-        id:'google-map-script',
-        googleMapsApiKey:"AIzaSyDsI5Nfl4xBcf9qsmtQAFzUkqMMmBC9WCw"
-        
-    })
+  const gigs = useGigs()
+  const currentDateMs = Date.now()
+  const gigsToday = getGigsToday(gigs,currentDateMs)
 
-    const center = {
-        lat:-41.294,
-        lng:174.777
-    }
+  console.log(gigsToday)
 
-    const containerStyle = {
-        width: '373px',
-        height: '410px',
-        borderRadius: '26px'
-      };
+  const center = {
+    lat: -41.294,
+    lng: 174.777,
+  };
 
-    const map = isLoaded ? (
-        <div className="map-container">
-    <GoogleMap
+  const containerStyle = {
+    width: "373px",
+    height: "410px",
+    borderRadius: "26px",
+  };
+
+  const map = isLoaded ? (
+    <div className="map-container">
+      <GoogleMap
         zoom={14}
-        center = {center}
+        center={center}
         mapContainerStyle={containerStyle}
-        options = {{styles:mapStyle}}
-        >
+        options={{ styles: mapStyle }}
+      >
+        {gigsToday.map((gig,i) => {
+            const lat = gig.location.latitude
+            const lng = gig.location.longitude
+            const location = {lat:lat,lng:lng}
+            return (
+                <Marker
+                position = {location}
+            ></Marker>
+            )
+        })}
+      </GoogleMap>
+    </div>
+  ) : (
+    <></>
+  );
 
-        </GoogleMap>
-        </div>
-    ) : <></>
+  return (
+    <div className="map">
+      <div className="logo">
+        <img src={logo} />
+      </div>
+      {map}
+    </div>
+  );
+};
 
-    return ( 
-        <div>{map}</div>
-     );
-}
- 
 export default Map;
