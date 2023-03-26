5e833e1d8d707303dc6b3a68ca99c05f442d64c4
Joshua Simon <joshy.d.simon@gmail.com>
2023-03-26 13:37:40 +1300

    map stuff

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState,useMemo } from "react";
 import { GoogleMap, useJsApiLoader,Marker } from "@react-google-maps/api";
 import { mapStyle } from "../util/mapStyle";
-import logo from "../assets/logo.png";
+import logo from "../assets/test2.png";
 import { getGigsToday } from "../util/helperFunctions";
 import { useGigs } from "../hooks/useGigs";
 import mapPin from "../assets/map-pin-new.png"
@@ -11,6 +11,7 @@ import { AiFillCaretLeft } from 'react-icons/ai'
 import { AiFillCaretRight } from 'react-icons/ai'
 
 const Map = () => {
+  const [ selectedDateMs,setSelectedDateMs ] = useState(Date.now())
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyDsI5Nfl4xBcf9qsmtQAFzUkqMMmBC9WCw",
@@ -18,20 +19,45 @@ const Map = () => {
 
   const gigs = useGigs()
   const currentDateMs = Date.now()
-  const gigsToday = getGigsToday(gigs,currentDateMs)
+
 
   const center = {
     lat: -41.294,
     lng: 174.777,
   };
 
-  const formattedDay = format(new Date(currentDateMs), "EEEE");
-  const formattedWeek = format(new Date(currentDateMs), "LLLL do Y");
+  const selectedDateString = useMemo(() => {
+    const formattedDate = format(new Date(selectedDateMs),'EEE LLL do Y')
+    return formattedDate
+  }, [selectedDateMs]);
+
+  const currentDay = useMemo(() => {
+    const formattedDay = format(new Date(selectedDateMs),'EEEE')
+    return formattedDay
+  },[selectedDateMs])
+
+  const currentWeek = useMemo(() => {
+    const formattedDay = format(new Date(selectedDateMs),'LLLL do Y')
+    return formattedDay
+  },[selectedDateMs])
+
+    //Filtering through gigs to return only current day's gigs
+    const gigsToday = gigs.filter((gig) => {
+      const formattedGigDate = format(new Date(gig.dateAndTime.seconds * 1000), 'EEE LLL do Y')
+      return formattedGigDate === selectedDateString;
+    });
+
+    const addDays = (amount) => {
+      setSelectedDateMs((curr) => curr + 1000 * 60 * 60 * 24 * amount);
+    };
+
+
 
   const containerStyle = {
-    width: "373px",
-    height: "410px",
+    width: "90%",
+    height: "450px",
     borderRadius: "26px",
+    margin: "0 auto"
   };
 
 
@@ -71,8 +97,8 @@ const Map = () => {
       </div>
 
       <div className="gigsToday_map">
-        <p className="gigsToday_date_day">{formattedDay}</p>
-        <p className="gigsToday_date_week">{formattedWeek}</p>
+        <p className="gigsToday_date_day">{currentDay}</p>
+        <p className="gigsToday_date_week">{currentWeek}</p>
       </div>
 
       <div className="map_text">
@@ -84,11 +110,11 @@ const Map = () => {
 
       <div className="arrows">
         <div className="arrow_left">
-          <AiFillCaretLeft size={48} />
+          <AiFillCaretLeft size={48} onClick ={() => addDays(-1)} />
           <p>Previous Day</p>
         </div>
         <div className="arrow_right">
-          <AiFillCaretRight size={48}/>
+          <AiFillCaretRight size={48} onClick ={() => addDays(1)}/>
           <p>Next Day</p>
         </div>
       </div>
