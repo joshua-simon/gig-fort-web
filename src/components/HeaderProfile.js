8f6c930b4f11920c6bcfb2ff6a521ac131d278c9
Joshua Simon <joshy.d.simon@gmail.com>
2023-09-12 10:24:35 +1200

    styling for profile page

---
+++ src/components/HeaderProfile.js
@@ -0,0 +1,8 @@
+
+const HeaderProfile = () => {
+    return (
+        <div style = {{backgroundColor: '#2596be'}}>Header profile</div>
+    )
+}
+ 
+export default HeaderProfile;
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,28 @@
+import HeaderProfile from "../components/HeaderProfile";
+import Footer from "../components/Footer"
+import logo from '../assets/logo_light_5.png'
+
 const Profile = () => {
+
+    const main = (
+        <div className="profile-main">
+            <div className="profile-main-header">
+                <p className="profile-main-header-name">Joshua Simon</p>
+                <p className="profile-main-header-location">Wellington</p>
+            </div>
+            <div className="profile-main-logo">
+                <img src = {logo}/>
+                <p>You haven't saved any gigs yet!</p>
+            </div>
+        </div>
+    )
+
     return ( 
-        <div>Profile</div>
+        <>
+            <HeaderProfile/>
+            {main}
+            <Footer/>
+        </>
      );
 }
  
