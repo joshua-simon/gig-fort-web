d5c30e4e6e9d937e2989662a0f31dbf7dd193261
Joshua Simon <joshy.d.simon@gmail.com>
2023-09-13 14:42:46 +1200

    delete logic working

--- src/screens/DeleteAccount.js
+++ src/screens/DeleteAccount.js
@@ -1,10 +1,26 @@
+import { deleteUser } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const DeleteAccount = () => {
+    const navigate = useNavigate()
+
+    const deleteUserAccount = () => {
+        deleteUser(auth.currentUser)
+          .then(() => {
+            alert('Account Deleted')
+            navigate("/");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
     return (
         <div className="delete-account-container">
             <p>Are you sure you want to delete your account?</p>
-            <button className="delete-account-container-delete-account-button">Delete Account</button>
-            <button className="delete-account-container-return-button">Return to profile</button>
+            <button className="delete-account-container-delete-account-button" onClick = {deleteUserAccount}>Delete Account</button>
+            <button className="delete-account-container-return-button" onClick = {() => navigate('/profile')}>Return to profile</button>
         </div>
     )
 }
